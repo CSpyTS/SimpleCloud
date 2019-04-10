a01326fe857cd8bd3006a464723c7f9e35a0912b
CSpyTS <toStrongest@163.com>
2019-04-10 21:47:12 +0800

    更新： 全部文件界面显示，基本完成 左侧侧边栏布局，基本完成

--- src/main/java/cspy/online/controller/MainFrameController.java
+++ src/main/java/cspy/online/controller/MainFrameController.java
@@ -6,6 +6,7 @@ import cspy.online.dao.FileMapper;
 import cspy.online.dao.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,16 +26,9 @@ import java.util.Objects;
 @Controller
 public class MainFrameController {
 
-    List<Path> paths;
 
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String mainFrame() {
-        if (Objects.isNull(paths)) {
-            paths = new ArrayList<>();
-        }
-        if (paths.size() == 0) {
-            paths.add(Paths.get("D:/SimpleCloud/users", "CSpy"));
-        }
         return "index";
     }
 
@@ -45,27 +39,40 @@ public class MainFrameController {
     private UserMapper userMapper;
 
 
-    @ModelAttribute("fileList")
-    public List<SCFile> getFileList() {
-        int uid = 1;
-        SCUser user = userMapper.selectOneByUid(uid);
-        System.out.println("username:" + user.getUsername());
-        Path path = Paths.get("D:/SimpleCloud/users", "CSpy");
-        System.out.println("path:" + path);
-        return fileMapper.getFileList(path);
+//    @ModelAttribute("fileList")
+//    public List<SCFile> getFileList() {
+//        int uid = 1;
+//        SCUser user = userMapper.selectOneByUid(uid);
+//        System.out.println("username:" + user.getUsername());
+//        Path path = Paths.get("D:/SimpleCloud/users", "CSpy");
+//        System.out.println("path:" + path);
+//        return fileMapper.getFileList(path);
+//    }
+
+
+    @RequestMapping("/getFileList")
+    public String getPath(@RequestParam("path") String path, Model model) {
+        List<SCFile> files = fileMapper.getFileList(Paths.get(path));
+        model.addAttribute("fileList", files);
+        System.out.println("controller path = " + path);
+//        for (SCFile scFile : files) {
+//            System.out.println(scFile.getFilename());
+//        }
+        return "index::file-table-body";
     }
 
-    @ModelAttribute("filePathList")
-    public List<Path> getFilePathList() {
-        return paths;
+//    @ModelAttribute("filePathList")
+//    public List<Path> getFilePathList() {
+//        return paths;
+//
+//    }
 
-    }
-
-    @RequestMapping("/main/addPath")
-    public void addPath(@RequestParam("path") String path) {
-        paths.add(Paths.get(path));
-
-    }
+//    @RequestMapping("/addPath")
+//    public String addPath(@RequestParam("path") String path, Model model) {
+//        paths.add(Paths.get(path));
+//        model.addAttribute("filePathList", path);
+//        return "main::file-path-item";
+//    }
 
 
 
